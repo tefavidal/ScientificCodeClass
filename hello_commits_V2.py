a470b7bd9e9d584fdb43290d77c602ed67ea4378
tefavidal <tefavidal@gmail.com>
2018-06-11 15:44:38 +0200

    MoreLines

---
+++ hello_commits_V2.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+if __name__ == "__main__":
+	print('Hello World')
+	print('OMG Second commit!')
+	print('Hello World again')
+	print('I think I already understood the basic concept of add and commit')
+	print('But how can git recognize it is the same file if both name and some of the content changed? Is there a limit?')
