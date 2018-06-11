e1af70031b31cb3850b7749925f804770931ae4d
tefavidal <tefavidal@gmail.com>
2018-06-11 15:47:54 +0200

    NoAdding

--- hello_commits_V2.py
+++ hello_commits_V2.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
 	print('Hello World again')
 	print('I think I already understood the basic concept of add and commit')
 	print('But how can git recognize it is the same file if both name and some of the content changed? Is there a limit?')
+	print('I think I already understood the basic concept of add and commit. Really?')
