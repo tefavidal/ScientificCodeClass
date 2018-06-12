994c2fe707e023f08c57a010c406e785eca34419
tefavidal <tefavidal@gmail.com>
2018-06-12 11:49:14 +0200

    No text no palm trees

--- hello_world.py
+++ hello_world.py
@@ -21,9 +21,6 @@ def print_palmtrees(num):
               sys.stdout.write('\n\n')
 
 if __name__ == "__main__":
-	print('Hello Rabbits')
-	print('I think I already understood the basic concept of add and commit')
-	print('But how can git recognize it is the same file if both name and some of the content changed? Is there a limit?')
-	print_monkeys([0,1,2,3,4,5])
-	print_palmtrees(10)
-	print_rabbit_range(7)
+    print('No more trees, only animals')
+    print_monkeys([0,1,2,3,4,5])
+    print_rabbit_range(15)
