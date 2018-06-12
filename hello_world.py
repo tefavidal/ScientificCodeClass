dbba250268adb4e32cd7c116b9ae8af7c3defce3
tefavidal <tefavidal@gmail.com>
2018-06-12 10:43:53 +0200

    Palm trees added to hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,18 @@
 from monkey import print_monkeys
+import sys
 # -*- coding:utf-8 -*-
+def print_palmtrees(num):
+              for i in range(num):
+                  sys.stdout.write(' /|\\')
+              sys.stdout.write('\n')
+              for i in range(num):
+                  sys.stdout.write('  | ')
+              sys.stdout.write('\n\n')
+
 if __name__ == "__main__":
 	print('Testing Changes!')
 	print('Hello World again')
 	print('I think I already understood the basic concept of add and commit')
 	print('But how can git recognize it is the same file if both name and some of the content changed? Is there a limit?')
 	print_monkeys([0,1,2,3,4,5])
-	print('Arbitrary change number one')
+	print_palmtrees(10)
